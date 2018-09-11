d12635c68ac4cbaba33d5115eee1b9b7dcc7f979
Thaison Dang <thaisondang.bk57@gmail.com>
2018-09-12 00:09:12 +0200

    sds

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return ( 
+          <div className = "App" >
+            <h1 className="bg-success display-1">Hello</h1>
+            <button className="btn btn-primary">skdnfsd</button>
+          </div>
+        );
+    }
 }
 
 export default App;
