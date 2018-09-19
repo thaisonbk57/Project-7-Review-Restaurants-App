a9b997e510dad5585b6309189552bebab40c99f5
Thaison Dang <thaisondang.bk57@gmail.com>
2018-09-19 20:33:39 +0200

    aaaaa

--- src/components/header/heading/Heading.js
+++ src/components/header/heading/Heading.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const heading = props => (
-  <h1 className="heading heading-primary display-2 text-center text-light">
+  <h1 className="heading heading-primary display-3 text-center text-light">
     {props.heading}
   </h1>
 );
