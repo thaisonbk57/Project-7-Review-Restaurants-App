63f34a6cf85261d1d11c8425685f9292f013f02e
Thaison Dang <thaisondang.bk57@gmail.com>
2018-10-05 13:37:09 +0200

    added close comment Form button

--- src/containers/addCommentForm/index.js
+++ src/containers/addCommentForm/index.js
@@ -1,43 +1,42 @@
-import React, {Component} from "react";
-import Name from "./name";
-import Comment from "./comment";
-import SubmitBtn from "./submmitBtn";
-import RatingStar from "./ratingStar/ratingStar";
-import {connect} from "react-redux";
-import "./commentForm.css";
-import {addComment} from "../../store/actions";
+import React, { Component } from 'react';
+import Name from './name';
+import Comment from './comment';
+import SubmitBtn from './submmitBtn';
+import RatingStar from './ratingStar/ratingStar';
+import { connect } from 'react-redux';
+import './commentForm.css';
+import { addComment } from '../../store/actions';
 
 const getTimeStamp = () => {
   // this function generate unique key for each review of users
   let now = new Date();
   return now.getTime();
-}
+};
 
 class addCommentForm extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      author_name: "",
+      author_name: '',
       rating: 5,
-      text: "",
+      text: ''
     };
   }
 
   onChangeHander = e => {
     let value = e.target.value;
     switch (e.target.name) {
-      case "author":
+      case 'author':
         this.setState({
           author_name: value
         });
         break;
-      case "comment_text":
+      case 'comment_text':
         this.setState({
           text: value
         });
         break;
-      case "star":
+      case 'star':
         this.setState({
           rating: parseInt(value, 10)
         });
@@ -45,36 +44,52 @@ class addCommentForm extends Component {
       default:
         return null;
     }
-  }
+  };
 
-  onSubmitHandler = (e) => {
+  onSubmitHandler = e => {
     e.preventDefault();
-  }
+  };
 
   render() {
-
     let form = null;
     if (this.props.active) {
-      form = <div className="form-backdrop">
-              <div 
-                autoComplete="off" 
-                className="AddCommentForm border border-success px-5 py-2 rounded"
-                onSubmit={(e)=> {this.onSubmitHandler(e)}} >
-
-                <label className="text-light" htmlFor="">Name: </label>
-                <Name onchange={this.onChangeHander} />
-                <br/>
-                <RatingStar onchange={this.onChangeHander} />
-                <br/>
-                <label className="text-light" htmlFor="">Comment: </label>
-                <Comment onchange={this.onChangeHander} />
-                <br/>
-                <SubmitBtn submit={() => {this.props.addComment({...this.state, time: getTimeStamp()}, this.props.targetRestaurant)}} />
-
-              </div>
-            </div>
+      form = (
+        <div className="form-backdrop">
+          <div className="close-btn">
+            <i class="far fa-window-close" />
+          </div>
+          <div
+            autoComplete="off"
+            className="AddCommentForm border border-success px-5 py-2 rounded"
+            onSubmit={e => {
+              this.onSubmitHandler(e);
+            }}
+          >
+            <label className="text-light" htmlFor="">
+              Name:{' '}
+            </label>
+            <Name onchange={this.onChangeHander} />
+            <br />
+            <RatingStar onchange={this.onChangeHander} />
+            <br />
+            <label className="text-light" htmlFor="">
+              Comment:{' '}
+            </label>
+            <Comment onchange={this.onChangeHander} />
+            <br />
+            <SubmitBtn
+              submit={() => {
+                this.props.addComment(
+                  { ...this.state, time: getTimeStamp() },
+                  this.props.targetRestaurant
+                );
+              }}
+            />
+          </div>
+        </div>
+      );
     }
-    return(form);
+    return form;
   }
 }
 
@@ -82,13 +97,18 @@ const mapState = state => {
   return {
     active: state.activeCommentForm,
     targetRestaurant: state.activeRestaurant
-  }
-}
+  };
+};
 
 const mapDispatch = dispatch => {
   return {
-    addComment: (commentObject, targetRestaurant) => {dispatch(addComment(commentObject, targetRestaurant))}
-  }
-}
+    addComment: (commentObject, targetRestaurant) => {
+      dispatch(addComment(commentObject, targetRestaurant));
+    }
+  };
+};
 
-export default connect(mapState, mapDispatch)(addCommentForm);
+export default connect(
+  mapState,
+  mapDispatch
+)(addCommentForm);
