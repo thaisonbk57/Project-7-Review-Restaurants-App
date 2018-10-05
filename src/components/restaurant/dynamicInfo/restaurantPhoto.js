a565da07fe30c190f5fe8aec6a196a19b8a129ff
Thaison Dang <thaisondang.bk57@gmail.com>
2018-10-05 12:45:20 +0200

    handle no photos available of carousel

--- src/components/restaurant/dynamicInfo/restaurantPhoto.js
+++ src/components/restaurant/dynamicInfo/restaurantPhoto.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React from 'react';
 
 const restaurantPhoto = props => {
   return (
     <div className="restaurant--photo">
-      <img className="d-block mw-100 p-2" src={props.restaurantPhoto} alt={"street photo of " + props.restaurantName} />
+      <img
+        className="d-block mw-100 p-2"
+        src={props.restaurantPhoto}
+        alt={'street photo of ' + props.restaurantName}
+      />
     </div>
   );
-}
+};
 export default restaurantPhoto;
