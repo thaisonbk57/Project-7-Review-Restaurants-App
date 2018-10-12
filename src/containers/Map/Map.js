d964c6674ccd33e29ea55f9bc6b2490944fcc629
Thaison Dang <thaisondang.bk57@gmail.com>
2018-10-12 11:11:40 +0200

    styling the map

--- src/containers/Map/Map.js
+++ src/containers/Map/Map.js
@@ -72,9 +72,6 @@ class MyMapComponent extends React.Component {
         case 18:
           radius = 200;
           break;
-        case 19:
-          radius = 100;
-          break;
         default:
           radius = 500;
       }
@@ -172,8 +169,104 @@ class MyMapComponent extends React.Component {
 
     return (
       <GoogleMap
-        defaultZoom={18}
+        defaultZoom={17}
         defaultCenter={userPos}
+        defaultOptions={{
+          fullscreenControl: false,
+          mapTypeControl: false,
+          maxZoom: 18,
+          minZoom: 15,
+          styles: [
+            { elementType: 'geometry', stylers: [{ color: '#535c68' }] },
+            {
+              elementType: 'labels.text.stroke',
+              stylers: [{ color: '#242f3e' }]
+            },
+            {
+              elementType: 'labels.text.fill',
+              stylers: [{ color: '#746855' }]
+            },
+            {
+              featureType: 'administrative.locality',
+              elementType: 'labels.text.fill',
+              stylers: [{ color: '#d59563' }]
+            },
+            {
+              featureType: 'poi',
+              elementType: 'labels.text.fill',
+              stylers: [{ color: '#d59563' }]
+            },
+            {
+              featureType: 'poi.business',
+              stylers: [{ visibility: 'off' }]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'geometry',
+              stylers: [{ color: '#263c3f' }]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'labels.text.fill',
+              stylers: [{ color: '#6b9a76' }]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry',
+              stylers: [{ color: '#38414e' }]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry.stroke',
+              stylers: [{ color: '#212a37' }]
+            },
+            {
+              featureType: 'road',
+              elementType: 'labels.text.fill',
+              stylers: [{ color: '#9ca5b3' }]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry',
+              stylers: [{ color: '#746855' }]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry.stroke',
+              stylers: [{ color: '#1f2835' }]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'labels.text.fill',
+              stylers: [{ color: '#f3d19c' }]
+            },
+            {
+              featureType: 'transit',
+              elementType: 'geometry',
+              stylers: [{ color: '#2f3948' }]
+            },
+            {
+              featureType: 'transit.station',
+              elementType: 'labels.text.fill',
+              stylers: [{ color: '#d59563' }]
+            },
+            {
+              featureType: 'water',
+              elementType: 'geometry',
+              stylers: [{ color: '#17263c' }]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.fill',
+              stylers: [{ color: '#515c6d' }]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.stroke',
+              stylers: [{ color: '#17263c' }]
+            }
+          ]
+        }}
         center={this.props.mapCenter.coords}
         ref={this.onMapMounted}
         onCenterChanged={() => {
