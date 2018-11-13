2da0e56dc5b9efd8841a1e744d62ae75899dadff
Thai Son Dang <thaisondang.bk57@gmail.com>
2018-11-13 12:02:46 +0100

    rebuild the project and modified README.MD

--- src/containers/Map/Carousel/Carousel.js
+++ src/containers/Map/Carousel/Carousel.js
@@ -33,43 +33,43 @@ class carousel extends React.Component {
     let photosCarousel = null;
     const { allPhotos, restaurantName } = this.props;
 
-    // if (allPhotos) {
-    //   photosCarousel = allPhotos.map((photo, index) => {
-    //     if (index === 0) {
-    //       // we need to active the first photo
-    //       return (
-    //         <div key={photo.photo_reference} className="carousel-item active">
-    //           <img
-    //             className="d-block"
-    //             style={{ width: "100%", height: 250 }}
-    //             src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=500&photoreference=${
-    //               photo.photo_reference
-    //             }&key=${API_KEY}`}
-    //             alt={`${restaurantName}`}
-    //           />
-    //         </div>
-    //       );
-    //     }
+    if (allPhotos) {
+      photosCarousel = allPhotos.map((photo, index) => {
+        if (index === 0) {
+          // we need to active the first photo
+          return (
+            <div key={photo.photo_reference} className="carousel-item active">
+              <img
+                className="d-block"
+                style={{ width: "100%", height: 250 }}
+                src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=500&photoreference=${
+                  photo.photo_reference
+                }&key=${API_KEY}`}
+                alt={`${restaurantName}`}
+              />
+            </div>
+          );
+        }
 
-    //     return (
-    //       <div key={photo.photo_reference} className="carousel-item">
-    //         <img
-    //           className="d-block"
-    //           style={{ width: "100%", height: 250 }}
-    //           src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=500&photoreference=${
-    //             photo.photo_reference
-    //           }&key=${API_KEY}`}
-    //           alt={`${restaurantName}`}
-    //         />
-    //       </div>
-    //     );
-    //   });
-    // }
+        return (
+          <div key={photo.photo_reference} className="carousel-item">
+            <img
+              className="d-block"
+              style={{ width: "100%", height: 250 }}
+              src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=500&photoreference=${
+                photo.photo_reference
+              }&key=${API_KEY}`}
+              alt={`${restaurantName}`}
+            />
+          </div>
+        );
+      });
+    }
 
     return (
       // using bootstrap 4 template for carousel
       <div id="carousel" className="carousel slide" data-ride="carousel">
-        {/* <div className="carousel-inner">{photosCarousel}</div> */}
+        <div className="carousel-inner">{photosCarousel}</div>
 
         <a className="carousel-control-prev" href="#carousel" data-slide="prev">
           <span className="carousel-control-prev-icon" />
